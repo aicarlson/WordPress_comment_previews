f7c6285a9a35e0e90b1abb96e18382fc82f1b03c
aicarlson <contact@ivanthevariable.com>
2013-11-14 13:46:47 -0600

    Updated JavaScript/jQuery
    This commit basically does a reset on existing value's in the comment
    form inputs and then shortens the function that watches for input
    changes.
    
    Efficiency and such.

--- commentpreview.js
+++ commentpreview.js
@@ -1,18 +1,17 @@
 jQuery(document).ready(function() {
-	jQuery('#author').change(function() {
-		var author = jQuery(this).val();
+	jQuery('.commentInput').each(function() {
+		jQuery(this).val('');
+	});
+	jQuery('.commentInput').change(function() { 
+		var author = jQuery('#author').val();
 		jQuery('.preview-url').text(author);
-	})
-	jQuery('#email').change(function() {
-		var email = CryptoJS.MD5( jQuery(this).val().toLowerCase().trim() );
+		var email = CryptoJS.MD5( jQuery('#email').val().toLowerCase().trim() );
 		jQuery('.commentPreview .avatar').attr('src','http://gravatar.com/avatar/' + email);
-	})
-	jQuery('#url').change(function() {
-		var url = jQuery(this).val();
+		var url = jQuery('#url').val();
 		jQuery('.preview-url').attr('href',url);
-	})
+	});
 	jQuery('textarea.comment').keyup(function() {
 		var comment = jQuery(this).val();
 		jQuery('.preview-text').html(comment);
-	})
-})
+	});
+});
